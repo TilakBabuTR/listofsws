6917c986f967732307ac225b68b30f5700245c89
TilakBabuTR <tilakbabu.bopparaju@thomsonreuters.com>
2020-05-15 14:07:20 +0530

    pre-commit modified

--- todo.py
+++ todo.py
@@ -17,6 +17,7 @@ class Todo:
 				print("Do you have task details?")
 				print("Is my githooks file working???")
 				print("Is my githooks file working???")
+				print("Is my githooks file working???")
 
 				# ID=int(input("task ID: "))
 				# c.execute("SELECT * from todo where taskid= taskid", {'taskid':ID})
